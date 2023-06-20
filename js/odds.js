d0c9738d5ee888dea57bde976c14bde836dea365
Simon <simon.hobbs@eyasgaming.com>
2023-06-20 22:37:41 +0200

    v1

---
+++ js/odds.js
@@ -0,0 +1,82 @@
+
+// usage: <inline-odds id="123456"></inline-odds>
+// 
+
+const template = document.createElement('template');
+
+template.innerHTML = `
+        <a href="url" target="lancebet" style="background: #F8F838;">N/A</a>
+  `
+
+class Odds extends HTMLElement {
+
+    // You can edit these:
+    prod = false;
+    affiliateId = "AY2838324479"
+
+    // Do not edit anyething below this line
+    nonProdUrl = "https://graphql.cts.kambicdn.com"
+    prodUrl = "https://graphql.kambicdn.com"
+
+    nonProdTarget = "https://lancebet-com-uat.eyasgaming.net"
+    prodTarget = "https://www.lancebet.com"
+
+    constructor() {
+        super();
+        this._shadowRoot = this.attachShadow({ mode: 'open' });
+        this._shadowRoot.appendChild(template.content.cloneNode(true));
+
+        this.url = this.prod ? this.prodUrl : this.nonProdUrl;
+        this.target = this.prod ? this.prodTarget : this.nonProdTarget;
+    }
+
+    get id() {
+        return this.getAttribute('id');
+    }
+
+    connectedCallback() {
+        this.$odds = this._shadowRoot.querySelector('a');
+        this.getOdds(this.id);
+    }
+
+    getOdds(id) {
+        const data = JSON.stringify({
+            query: `
+                query Outcome {
+                    outcomes(offering: "eyasgamingbr", market: "BR", outcomeIds: "` + id + `") {
+                        outcomes {
+                            odds
+                            status
+                        }
+                    }
+            }`
+        });
+
+        const response = fetch(
+            this.url,
+            {
+                method: 'post',
+                body: data,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Content-Length': data.length,
+                    Authorization:
+                        'eyas_gaming',
+                },
+            }
+        ).then((response) => response.json())
+            .then((data) => this.renderPrice(data.data.outcomes.outcomes[0].odds))
+    }
+
+    renderPrice(price) {
+        console.log("prod: " + this.prod)
+        console.log("url: " + this.url)
+        console.log("target: " + this.target)
+
+        this.$odds.innerHTML = (Number(price) / 1000).toFixed(2);
+        this.$odds.href = this.target + "?affiliateId" + this.affiliateId + "&coupon=single|" + this.id + "||append|lancebet";
+    }
+
+}
+
+window.customElements.define('inline-odds', Odds);
